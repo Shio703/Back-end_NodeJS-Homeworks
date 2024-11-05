7546e77995b69f698f6fb63cc3043c773f30ed7b
Shio703 <shiodjamurashvili26@gmail.com>
2024-11-06 01:58:29 +0400

    i'm a sleepy right now and i'll done it tommorow:)

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello NodeJS!");
+// CommonJS Syntax import statement
+// const asciiArt = require("ascii-art");
+// ECMA Script Modules Syntax
+import asciiArt from "ascii-art";
+
+asciiArt.font("Hello NodeJS!", "doom", (err, rendered) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(rendered);
+    console.log("Rendered Successfully:)");
+  }
+});
+// i don't know why but my text is displayng only when err = true
+//  i'm assuming it's internal bag of ascii-art package
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Homework Assignments from Back-end NodeJS Course
+
+```javascript
+console.log("Hello NodeJS!");
+import knowledge from "NH Back-end Programming with NodeJS";
+```
+
+<hr/>
+<p>this repository represents all the assigments which is included in this course!</p>
