d70ebbb4d26f8215c7df2273f96dd632dc0a7d99
Shio703 <shiodjamurashvili26@gmail.com>
2024-11-24 15:21:37 +0400

    added some features

--- homework_3/exec.js
+++ homework_3/exec.js
@@ -1,9 +1,16 @@
 import { exec } from "node:child_process";
 import { createWriteStream } from "node:fs";
+import { exit } from "node:process";
 
 const resultarr = [];
 const date = new Date().toLocaleDateString();
 const time = new Date().toLocaleTimeString();
+const fileName = process.argv[2];
+
+if (!fileName) {
+  console.log("Provide Output File Name! as a first argument");
+  exit(1);
+}
 
 exec(
   "echo %os% && echo %username% && echo %windir% && echo %number_of_processors% && echo %computername%",
@@ -27,7 +34,7 @@ exec(
     const toJson = JSON.stringify(resultObj);
 
     //  writing the result data into a json file using writable stream
-    const WritableStream = createWriteStream("./output.json", {
+    const WritableStream = createWriteStream(`./${fileName}.json`, {
       encoding: "utf-8",
     });
     WritableStream.write(toJson);
