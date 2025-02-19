3b8d660afcf5e706dd898005030e6696beef2649
Shio703 <shiodjamurashvili26@gmail.com>
2025-02-19 16:54:27 +0400

    fixed bug with debugger and implemented another test case for new function 'numChecker'

--- homework_5/samples/task2.js
+++ homework_5/samples/task2.js
@@ -1,27 +1,37 @@
 const test = require("node:test");
 const assert = require("node:assert");
 
+const numChecker = (array) => {
+  return array.some((item) => {
+    return typeof item !== "number";
+  });
+};
+
 function calculateSum(numbers) {
   if (numbers.length < 2) {
     return "Provide an array with minimum 2 element!";
-  } else if (typeof numbers !== "number") {
+  } else if (numChecker(numbers)) {
     return "Provide Number values only!";
+  } else {
+    return numbers.reduce((total, a, b) => {
+      return (total += a + b);
+    });
   }
-
-  return numbers.reduce((a, b) => a + b);
 }
-
-const testArray = [10, 37, "v"];
+// if you provide an array with different types of data except number you'll get an exception
+const testArray = [10, 37, 50, 70, 15, 24, 77, "feria"];
 
 // test checks if result of the function is number
 test.test("Function executes successfully", () => {
   console.log(calculateSum(testArray));
 });
+// this test checks if function trows an exception in case it gets a string values instead of number
 test.test("Function handles NaN cases", () => {
   assert.strictEqual(typeof calculateSum(testArray), "string");
 });
-test.test(
-  "Function",
-  { todo: "i should think of this test case", skip: "not implemented yet!" },
-  () => {}
-);
+// this test checks if nuChecker works fine return value must be a boolean
+test.test("Function numChecker works fine", () => {
+  assert.doesNotThrow(() => {
+    numChecker(testArray);
+  });
+});
