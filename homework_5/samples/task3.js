2d403f26f0bfc884ffd53350b8062631931d98fa
Shio703 <shiodjamurashvili26@gmail.com>
2025-02-22 01:08:33 +0400

    version 1.0.0 of this task is done

--- homework_5/samples/task3.js
+++ homework_5/samples/task3.js
@@ -1,3 +1,6 @@
+const { test } = require("node:test");
+const assert = require("node:assert");
+
 function findLargestNumber(arr) {
   if (arr.length < 2) {
     return "Provide an array with minimum 2 number!";
@@ -16,5 +19,15 @@ function findLargestNumber(arr) {
 const numbers = [
   1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35,
 ];
+const messyArray = [1, 3, 5, 7, "", 15, 17, 19, 21, 23, 25, 27, 29, 31, {}, 35];
 
 console.log("Largest number:", findLargestNumber(numbers));
+
+test("Function Handles empty array cases", () => {
+  typeof findLargestNumber([]) === "string";
+});
+
+test("Function handles arrays with different data types", () => {
+  // if my function fails in case not every item of the array is not a number it definetely trow an exception which is "string" differently from an error which is "object" in any case.
+  assert.strictEqual(typeof findLargestNumber(messyArray), "string");
+});
