cdb5d86b48ea3c41e2931e594f6bac3dd6ad8dc9
Shio703 <shiodjamurashvili26@gmail.com>
2025-03-03 03:56:37 +0400

    task4.js upload with console.time && console.timeEnd

---
+++ homework_5/samples/task4.js
@@ -0,0 +1,13 @@
+console.time("Slow function time");
+function slowFunction(n) {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      sum += i + j;
+    }
+  }
+  return sum;
+}
+
+console.log("Result:", slowFunction(10000));
+console.timeEnd("Slow function time");
